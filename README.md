68163ff44ab6691e573aa3404af122db71affc9f
isthatcentered <e.peninb@gmail.com>
2020-01-11 15:53:00 +0100

    feat(custom action): Custom hello world action

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Github actions hello world
+# Hello world javascript action
+
+Greet a person even she doesn't want to be
+
+## Inputs
+
+### `person`
+
+**Required** The name of the person to greet.
+
+## Outputs
+
+### `time`
+
+The time we greeted you.
+
+## Example usage
+
+uses: isthatcentered/github-actions-poc@v1
+with:
+  person: "Kanye"
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import log from "@isthatcentered/log";
+
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  // `person` input defined in action metadata file
+  const nameToGreet = core.getInput("person");
+  console.log(`Hello ${nameToGreet}!`);
+  const time = new Date().toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  log(`Event payload`)(payload);
+  log("Github Context")(github.context);
+} catch (error) {
+  core.setFailed(error.message);
+}
